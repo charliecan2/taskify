4b5657c4df41945c68897d5e5b8512086dc42560
ccantu20 <carloscq920@gmail.com>
2022-01-26 15:17:00 -0600

    Added functionality to the todo input bar. For now, it can only console log the tasks added.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from './components/InputField';
+import { Todo } from './model'
 
 import './App.css';
 
 const App: React.FC = () => {
-  return (
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAdd = (e: React.FormEvent<EventTarget>) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {
+        id: Date.now(),
+        todo: todo,
+        isDone: false
+      }]);
+      setTodo("");
+    }
+  }
+
+  console.log(todo);
+
+  return ( 
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import './styles.css'
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent<EventTarget>) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-  <form className='input'>
-    <input type='input' placeholder='Enter a task' className='inputBox'/>
+  <form className='input' onSubmit={handleAdd}>
+    <input 
+      type='input' 
+      placeholder='Enter a task' 
+      className='inputBox'
+      value={todo}
+      onChange={e => setTodo(e.target.value)}
+      />
     <button className='inputSubmit' type='submit'>
       Go
     </button>
